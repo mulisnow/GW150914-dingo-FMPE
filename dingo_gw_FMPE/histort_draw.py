67d711c1659d37988b82ac99331b9c604096627e
mulisnow <3130957143@qq.com>
2025-04-29 10:54:48 +0800

    反演参数后验分布的 corner 图像

--- dingo_gw_FMPE/histort_draw.py
+++ dingo_gw_FMPE/histort_draw.py
@@ -8,6 +8,7 @@ data = np.loadtxt(filename, delimiter="\t")
 # Plot loss values
 plt.plot(data[:,0], data[:,1], label=f"training loss")
 plt.plot(data[:,0], data[:,2], label=f"validation loss")
+plt.plot(data[:,0], data[:,3], label=f"loss")
 plt.xlabel("Epoch")
 plt.ylabel("Loss")
 plt.legend();
